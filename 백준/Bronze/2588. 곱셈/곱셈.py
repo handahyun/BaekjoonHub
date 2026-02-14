630cc7d1f62a7a1332162cecac489b4cd14faf46
dazigeum <96718120+handahyun@users.noreply.github.com>
2026-02-14 22:08:00 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -2,13 +2,13 @@ int1 = int(input())
 int2 = int(input())
 
 # 5
-num1 = int(int1* (int2 %10))
+num1 = int(int1 * (int2 % 10))
 # 85-5
-num2 = int((int1* (int2 %100-int2 %10)/10))
+num2 = int((int1 * (int2 % 100 - int2 % 10) / 10))
 # 385 - 85
-num3 = int((int1 *(int2 %1000-int2 %100)/100))
+num3 = int((int1 * (int2 % 1000 - int2 % 100) / 100))
 
-result = int1*int2
+result = int1 * int2
 
 print(num1)
 print(num2)
